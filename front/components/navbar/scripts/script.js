9e83ad22fd649f622b2859e144d5e039168007a4
viniciusmaier <viniciusfiel1000@gmail.com>
2025-10-24 02:27:28 -0300

    wip

--- front/components/navbar/scripts/script.js
+++ front/components/navbar/scripts/script.js
@@ -1,43 +1,59 @@
-const menuList = [
-    {
-        description: "Produtos",
-        isActived: false,
-        route: "/front/pages/products/list.html",
-    },
-    {
-        description: "Pedidos",
-        isActived: false,
-        route: "/front/pages/orders/list.html",
-    },
-];
-
-function handleEvent(e, option) {
-    window.location.assign(option.route);
-}
-
-function initNavBarMenu(componentsRoot) {
-    document.getElementById("content-menu").append(
-        ...menuList.map((option) => {
-            const li = document.createElement("li");
-            li.textContent = option.description;
-            li.addEventListener("click", (e) => handleEvent(e, option));
-            return li;
-        })
-    );
-}
-
 class NavbarComponent extends HTMLElement {
-    connectedCallback() {
-        fetch("/front/components/navbar/navbar.html")
-            .then((res) => res.text())
-            .then((html) => {
-                const template = document.createElement("template");
-                template.innerHTML = html;
-                const clone = template.content.cloneNode(true);
-                this.appendChild(clone);
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: "open",
+        });
+    }
 
-                initNavBarMenu(this);
-            });
+    menuList = [
+        {
+            description: "Produtos",
+            isActived: false,
+            route: "/front/pages/products/list.html",
+        },
+        {
+            description: "Pedidos",
+            isActived: false,
+            route: "/front/pages/orders/list.html",
+        },
+    ];
+
+    async connectedCallback() {
+        const archiveHtml = await fetch("/front/components/navbar/navbar.html");
+        const cssFile = await fetch("/front/components/navbar/index.css");
+        const html = await archiveHtml.text();
+        const css = await cssFile.text();
+
+        this.shadowRoot.innerHTML;
+
+        const template = document.createElement("template");
+        template.innerHTML = `
+        <style>${css}</style>
+        ${html}
+        `;
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.initNavBarMenu();
+    }
+
+    handleEvent(e, option) {
+        window.location.assign(option.route);
+    }
+
+    initNavBarMenu() {
+        const menu = this.shadowRoot.getElementById("content-menu");
+
+        menu.append(
+            ...this.menuList.map((option) => {
+                const li = document.createElement("li");
+                li.textContent = option.description;
+                li.addEventListener("click", (e) =>
+                    this.handleEvent(e, option)
+                );
+                return li;
+            })
+        );
     }
 }
 
